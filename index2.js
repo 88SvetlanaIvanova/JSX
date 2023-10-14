9cfd6b874c205cc0c683f3544a802d857c56311f
88SvetlanaIvanova <96539287+88SvetlanaIvanova@users.noreply.github.com>
2023-10-14 23:07:09 +0300

    Create index2.js
    Create a react app from scratch.
    It should display a h1 heading.
    It should display an unordered list (bullet points).
    It should contain 3 list elements.

---
+++ index2.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Heading</h1>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
